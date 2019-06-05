6e871814616a3f92f09414916dd6f712cceceeab
cg <cgallen@pm.me>
2019-06-06 07:58:36 +0800

    add container

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-if (window.innerWidth >=768) {
-    new fullpage('#fullpage', {
-        scrollingSpeed: 550,
-        loopTop: true,
-        loopBottom: true,
-        navigation: true,
-        navigationTooltips: ['Index', 'fff', 'ttt'],
-    });
-}   
+new fullpage('#fullpage', {
+    scrollingSpeed: 550,
+    loopTop: true,
+    loopBottom: true,
+    navigation: true,
+    responsiveWidth: 768
+});
